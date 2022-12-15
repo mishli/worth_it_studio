c61cd18a1f73e78a149edde88ba7b8915be760ed
Doron Mishli <doron@mish.li>
2022-12-15 22:38:49 +0200

    menu & pages

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@
 const socialImages = require("@11tyrocks/eleventy-plugin-social-images");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 // Helper packages
 const slugify = require("slugify");
@@ -12,56 +13,59 @@ const markdownItAnchor = require("markdown-it-anchor");
 const packageVersion = require("./package.json").version;
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(socialImages);
-  eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(pluginRss);
+	eleventyConfig.addPlugin(socialImages);
+	eleventyConfig.addPlugin(syntaxHighlight);
+	eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  eleventyConfig.addWatchTarget("./src/sass/");
+	eleventyConfig.addWatchTarget("./src/sass/");
+	eleventyConfig.addWatchTarget("./src/scripts/");
 
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy("./src/fonts");
-  eleventyConfig.addPassthroughCopy("./src/img");
-  eleventyConfig.addPassthroughCopy("./src/favicon.png");
+	eleventyConfig.addPassthroughCopy("./src/scripts");
+	eleventyConfig.addPassthroughCopy("./src/css");
+	eleventyConfig.addPassthroughCopy("./src/fonts");
+	eleventyConfig.addPassthroughCopy("./src/img");
+	eleventyConfig.addPassthroughCopy("./src/favicon.png");
 
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-  eleventyConfig.addShortcode("packageVersion", () => `v${packageVersion}`);
+	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+	eleventyConfig.addShortcode("packageVersion", () => `v${packageVersion}`);
 
-  eleventyConfig.addFilter("slug", (str) => {
-    if (!str) {
-      return;
-    }
+	eleventyConfig.addFilter("slug", (str) => {
+		if (!str) {
+			return;
+		}
 
-    return slugify(str, {
-      lower: true,
-      strict: true,
-      remove: /["]/g,
-    });
-  });
+		return slugify(str, {
+			lower: true,
+			strict: true,
+			remove: /["]/g
+		});
+	});
 
-  /* Markdown Overrides */
-  let markdownLibrary = markdownIt({
-    html: true,
-  }).use(markdownItAnchor, {
-    permalink: markdownItAnchor.permalink.ariaHidden({
-      class: "tdbc-anchor",
-      space: false,
-    }),
-    level: [1, 2, 3],
-    slugify: (str) =>
-      slugify(str, {
-        lower: true,
-        strict: true,
-        remove: /["]/g,
-      }),
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
+	/* Markdown Overrides */
+	let markdownLibrary = markdownIt({
+		html: true
+	}).use(markdownItAnchor, {
+		permalink: markdownItAnchor.permalink.ariaHidden({
+			class: "tdbc-anchor",
+			space: false
+		}),
+		level: [1, 2, 3],
+		slugify: (str) =>
+			slugify(str, {
+				lower: true,
+				strict: true,
+				remove: /["]/g
+			})
+	});
+	eleventyConfig.setLibrary("md", markdownLibrary);
 
-  return {
-    passthroughFileCopy: true,
-    dir: {
-      input: "src",
-      output: "public",
-      layouts: "_layouts",
-    },
-  };
+	return {
+		passthroughFileCopy: true,
+		dir: {
+			input: "src",
+			output: "public",
+			layouts: "_layouts"
+		}
+	};
 };
