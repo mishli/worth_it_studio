57681f6be74cddcc28ca4504cf2e938a010e2505
Doron Mishli <doron@mish.li>
2022-12-18 00:20:08 +0200

    footer, favicon and some typography

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,9 +1,8 @@
 module.exports = {
 	url: process.env.URL || "http://localhost:8080",
-	siteName: "WORTH IT | הדס משלי | סטודיו ביתי לאימוני כושר ממכרים",    
+	siteName: "Worth It Studio | Fitness & Happines",    
 	siteDescription: "סטודיו לכושר במרכז ת״א, אימונים אישיים ובקבוצות קטנות באווירה ממכרת. בואו להכיר את הסטודיו החדש שלכם שירגיש כמו בית! נתחזק ונהנה ביחד!",
 	author: "Doron Mishli",
-	authorTwitter: "@mish_li",
 	buildTime: new Date(),
 	languages: [
 		{
