a56a13ab875cc55f2854e4a6a875f0aef65a7239
Doron Mishli <doron@mish.li>
2023-01-02 21:12:47 +0200

    animation improves, contact forms regex's

--- src/scripts/the-studio.js
+++ src/scripts/the-studio.js
@@ -4,31 +4,25 @@
 
 (function() {
     // The Studio Main Title
-    theStudioMainTitleElm = document.getElementById('the-studio-main-title'),    
-    watcher_theStudio_main_title = scrollMonitor.create(theStudioMainTitleElm),
+    theStudioMainTitleElm = document.getElementById('the-studio-main-title'),
     theStudioMainTitle = new RevealFx(theStudioMainTitleElm, {
         isContentHidden: false,
         revealSettings : {
-            bgcolor: getComputedStyle(theStudioMainTitleElm).getPropertyValue('--color-second'),
+            bgcolor: getComputedStyle(theStudioMainTitleElm).getPropertyValue('--color-third'),
             onCover: function(contentEl, revealerEl) {
                 contentEl.style.opacity = 1;
-            },
-            onComplete: function() {
                 theStudioMainTitleElm.classList.add("title-bg","yellow");
             }
         }
     }),
-
-    watcher_theStudio_main_title.enterViewport(function() {
-        theStudioMainTitle.reveal();
-        watcher_theStudio_main_title.destroy();
-    });
+    
+    theStudioMainTitle.reveal();    
 
     // The Studio Sub Title 1
     theStudioSubTitle1Elm = document.getElementById('the-studio-sub-1'),    
     watcher_theStudio_sub_title1 = scrollMonitor.create(theStudioSubTitle1Elm, -150),
     theStudioSubTitle1 = new RevealFx(theStudioSubTitle1Elm, {
-        isContentHidden: false,
+        isContentHidden: false,        
         revealSettings : {
             bgcolor: getComputedStyle(theStudioSubTitle1Elm).getPropertyValue('--color-main'),
             onCover: function(contentEl, revealerEl) {
