4d0d91e8c18b3ac3506102def39954e853720be0
Doron Mishli <doron@mish.li>
2023-02-15 17:33:52 +0200

    blog order

--- .eleventy.js
+++ .eleventy.js
@@ -49,8 +49,8 @@ module.exports = function (eleventyConfig) {
         // return collection.getFilteredByGlob("./src/posts/*.njk");
 
         return collection.getFilteredByGlob("./src/posts/*.njk").sort(function(a, b) {
-            return a.date - b.date; // sort by date - ascending
-            // return b.date - a.date; // sort by date - descending
+            // return a.date - b.date; // sort by date - ascending
+            return b.date - a.date; // sort by date - descending
             //return a.inputPath.localeCompare(b.inputPath); // sort by path - ascending
             //return b.inputPath.localeCompare(a.inputPath); // sort by path - descending
           });
